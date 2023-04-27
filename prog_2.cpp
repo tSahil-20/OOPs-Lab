09d0b8942ab3a92ecd54c7c3fe7d61561bfda864
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2023-04-27 10:05:24 +0530

    #01.2

---
+++ prog_2.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class Circle{
+    private:
+        int radius;
+    public:
+        public:
+        float area(){
+            return 2*(3.14)*radius*radius;
+        }
+        float perimeter(){
+            return 2*3.14*radius;
+        }
+        void setRadius(int r){
+            radius = r;
+        }
+};
+
+int main() {
+    // Write C++ code here
+    Circle c;
+    c.setRadius(5);
+    cout << "\nArea of circle: "<< c.area();
+    cout << "\nPerimeter of circle: "<< c.perimeter();
+
+    return 0;
+}
